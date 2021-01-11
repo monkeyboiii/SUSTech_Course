9553b05ff04dedecc2a7df67e4a3abd258bf03ad
monkey <1283045105@qq.com>
2021-01-11 23:33:02 +0800

    simplified

--- main.py
+++ main.py
@@ -24,24 +24,7 @@ def login():
             '_eventId': 'submit',
             'geolocation': ''
         }
-        
-        r = s.post(url=url, data=data, headers={
-            # trivial
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "accept-encoding": "gzip, deflate, br",
-            "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-            "cache-control": "max-age=0",
-            "content-type": "application/x-www-form-urlencoded",
-            "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "same-origin",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1",
-            "referer": "https://cas.sustech.edu.cn/cas/login?service=https%3A%2F%2Ftis.sustech.edu.cn%2FcasLogin",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-        })
+        r = s.post(url=url, data=data)
         
         if r.status_code == 200:
             print(data['username'] + ' logged in!')
@@ -52,31 +35,25 @@ def login():
 
 
 def tisCourseCenter():
-    print(tisClick())
+    global config
     
-def tisClick():
+    forms = config['course_forms']
+    tasks = len(forms)
+    succees = [False * tasks]
+    task = 0
+    
+    while tasks > 0:
+        if not succees[task] and tisClick(forms[task]):
+            tasks = tasks - 1
+        else:
+            task = (task + 1) % tasks
+        time.sleep(0.1 + random.random() / 5)
+            
+    
+def tisClick(data):
     url='https://tis.sustech.edu.cn/Xsxk/addGouwuche'
-    data = 'p_pylx=1&p_sfgldjr=0&p_sfredis=0&p_sfsyxkgwc=0&p_xktjz=rwtjzyx&p_chaxunxh=&p_gjz=%E6%95%B0%E6%8D%AE%E6%8C%96%E6%8E%98&p_skjs=&p_xn=2020-2021&p_xq=2&p_xnxq=2020-20212&p_dqxn=2020-2021&p_dqxq=2&p_dqxnxq=2020-20212&p_xkfsdm=kzyxk&p_xiaoqu=&p_kkyx=&p_xkxs=&p_id=B3B8FB81A1A810EDE053CA0412AC70A7&p_sfhlctkc=0&p_sfhllrlkc=0&p_kxsj_xqj=&p_kxsj_ksjc=&p_kxsj_jsjc=&p_kcdm_js=&p_kcdm_cxrw=&p_kc_gjz=&p_xzcxtjz_nj=&p_xzcxtjz_yx=&p_xzcxtjz_zy=&p_xzcxtjz_zyfx=&p_xzcxtjz_bj=&p_sfxsgwckb=1&p_skyy=&p_chaxunxkfsdm=&pageNum=1&pageSize=13'
-    r = s.post(url=url,data=data,headers={
-        "content-type": "application/x-www-form-urlencoded",
-        
-        # trivial
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-        "cache-control": "max-age=0",
-        "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "same-origin",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-    })
-
+    r = s.post(url=url,data=data,headers={"content-type": "application/x-www-form-urlencoded"})
     print(r.text)
-    
     result = json.loads(r.text)
     if result['jg'] == '-1':
         return False
